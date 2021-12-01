066017c2e8c2f636617c7daeae2f25758a9cb0ca
zimingL <ziming.sc@gmail.com>
2021-12-01 22:18:04 +0100

    first commit

---
+++ src/test/java/dk/ku/convert/ConvertApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.ku.convert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
