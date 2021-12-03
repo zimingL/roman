7d0053d72b62fd2707c5cd7c785b05ee1d1ec56d
zimingL <ziming.sc@gmail.com>
2021-12-03 02:01:57 +0100

    ArabicToRomanService, test and implement

--- src/main/java/dk/ku/convert/service/ArabicToRomanService.java
+++ src/main/java/dk/ku/convert/service/ArabicToRomanService.java
@@ -1,22 +1,32 @@
 package dk.ku.convert.service;
 
-import java.util.HashMap;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class ArabicToRomanService {
-	public boolean isLegal(String romanN) {
+	public boolean isLegal(Integer arabicN) {
 		boolean isValidate = true;
-		Pattern pattern = Pattern.compile("^(?=[MDCLXVI])M*(C[MD]|D?C{0,3})(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
-	    Matcher matcher = pattern.matcher(romanN);
-	    isValidate = matcher.matches();
+		if (!(arabicN > 0 && arabicN < 4000))
+			isValidate = false;
 		return isValidate;		
 	}
-	
 
-	public String convert(Integer input) {
-		// TODO Auto-generated method stub
-		return null;
+	public String convert(Integer arabicN) {
+		if(!this.isLegal(arabicN))
+			return null;
+		StringBuilder result = new StringBuilder();
+		Integer times = 0;
+		String[] romanLetters = new String[] {"I", "IV", "V", "IX", "X", "XL", "L", "XC",
+				"C", "CD", "D", "CM", "M"};
+		Integer[] arabicNumbers = new Integer[] {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500,
+	            900, 1000 };
+		for (int i = arabicNumbers.length - 1; i >= 0; i--) {
+			times = arabicN / arabicNumbers[i];
+			arabicN %= arabicNumbers[i];
+			while(times > 0) {
+				result.append(romanLetters[i]);
+				times--;
+			}
+		}
+		return result.toString();
+		
 	}
 	
 }
--- src/main/java/dk/ku/convert/service/RomanToArabicService.java
+++ src/main/java/dk/ku/convert/service/RomanToArabicService.java
@@ -1,18 +1,22 @@
 package dk.ku.convert.service;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 public class RomanToArabicService {
-	public boolean isLegal(Integer arabicN) {
+	public boolean isLegal(String romanN) {
 		boolean isValidate = true;
-		if (!(arabicN > 0 && arabicN < 4000))
-			isValidate = false;
+		Pattern pattern = Pattern.compile("^(?=[MDCLXVI])M*(C[MD]|D?C{0,3})(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
+	    Matcher matcher = pattern.matcher(romanN);
+	    isValidate = matcher.matches();
 		return isValidate;		
 	}
 
 	public Integer convert(String romanN) {
-	 	// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
 		return null;
 	}
 
