eb62fc12cb1f86cdf95bc2ab97322a17400d29f2
zimingL <ziming.sc@gmail.com>
2021-12-03 02:19:03 +0100

    RomanToArabicService convert, test and implement

--- src/main/java/dk/ku/convert/service/RomanToArabicService.java
+++ src/main/java/dk/ku/convert/service/RomanToArabicService.java
@@ -1,5 +1,6 @@
 package dk.ku.convert.service;
 
+import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,8 +17,32 @@ public class RomanToArabicService {
 	}
 
 	public Integer convert(String romanN) {
-		// TODO Auto-generated method stub
-		return null;
+		if(!this.isLegal(romanN))
+			return null;
+		Integer result = 0;
+		HashMap<Character, Integer>romanMap = new HashMap<Character, Integer>(); // a map that contains all letters in Roman numeral
+		romanMap.put('I', 1);
+		romanMap.put('V', 5);
+		romanMap.put('X', 10);
+		romanMap.put('L', 50);
+		romanMap.put('C', 100);
+		romanMap.put('D', 500);
+		romanMap.put('M', 1000);
+		for(int i = 0; i < romanN.length(); i++) {
+			if (i == romanN.length() - 1) {
+				result += romanMap.get(romanN.charAt(i));
+				break;
+			}
+			if (romanMap.get(romanN.charAt(i)) >= romanMap.get(romanN.charAt(i + 1)) ) {
+				result += romanMap.get(romanN.charAt(i));
+			}
+			else if(romanMap.get(romanN.charAt(i)) < romanMap.get(romanN.charAt(i + 1)) ) {
+				result = result - romanMap.get(romanN.charAt(i)) + romanMap.get(romanN.charAt(i + 1));
+				i++;
+			}
+		}
+		return result;
+		
 	}
 
 	
